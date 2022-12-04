1ba09dc790bef1d3c45669a7b7e5dd7bd31102b2
gloriakamidi <g.kamidi.312@westcliff.edu>
2022-12-03 20:48:33 -0800

    w6d3lab

--- m3-simplekitchen-app/app.js
+++ m3-simplekitchen-app/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: true }));
